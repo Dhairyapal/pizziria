9e85f468e7ca56ad53a328e1e1e7ff7ea6b5ba51
Dhairyapal <78430771+Dhairyapal@users.noreply.github.com>
2021-06-09 10:27:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+menu_list_array = ["Veg Margherita Pizza","Double cheese pizza","Mexican Green Wave","Peppy Paneer","Cheese pizza",];
+document.getElementById
+function getmenu(){
+var get_menu=document.getElementById("add_item").value;
+menu_list_array.push(get_menu);
+document.getElementById("display_menu").innerHTML=menu_list_array;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
